4c1bbc2d2d307aba7a48d64ac4a30c12108b2508
ahmedGDI <guedria.guedri@gmail.com>
2023-08-10 21:43:19 +0100

    updating html pages

---
+++ KeepTrack.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  var signupButton = document.getElementById("signup-button");
+  signupButton.addEventListener("click", function() {
+    window.location.href = "signup.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var signupButton = document.getElementById("signup-button1");
+  signupButton.addEventListener("click", function() {
+    window.location.href = "signup.html";
+  });
+});
